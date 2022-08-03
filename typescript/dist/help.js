e8cbe57dae5a4e3bf8ff01dc2a4c50fe41b3f3c2
Norinot <naruto010190@gmail.com>
2022-08-03 14:34:22 +0200

    first commit

---
+++ typescript/dist/help.js
@@ -0,0 +1,11 @@
+"use strict";
+let ulr = 'ubb';
+let randomNum = 30;
+let bool = false;
+let randomObject = {};
+let numberArray = [1, 2, 34];
+let numberArrayV2 = [1, 2, 34];
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+addNumbers(15, 30);
---
+++ typescript/src/help.ts
@@ -0,0 +1,16 @@
+let ulr: string = 'ubb';
+let randomNum: number = 30;
+let bool: boolean = false;
+let randomObject: any = {
+
+}
+
+let numberArray: number[] = [1, 2, 34];
+let numberArrayV2: Array<number> = [1, 2, 34];
+
+function addNumbers(num1: number, num2: number): number {
+
+    return num1 + num2;
+}
+
+addNumbers(15, 30);
